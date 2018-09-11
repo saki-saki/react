8e6255d2c09108c304d105fd19dc68ca51262e2a
Levi Thomason <me@levithomason.com>
2018-09-11 15:18:15 -0700

    wip

--- docs/src/prototypes/ButtonTheme.tsx
+++ docs/src/prototypes/ButtonTheme.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import { NavLink } from 'react-router-dom'
-import { Button, Icon, Text } from '@stardust-ui/react'
+import { Button, Icon, Provider, Text, themes } from '@stardust-ui/react'
+import { IThemeInput } from '../../../types/theme'
 
 const VerticalStack = ({ gap, horizontalAlign, ...rest }: any) => (
-  <div style={{ display: 'flex', flexDirection: 'column' }} {...rest} />
+  <div style={{ padding: '1rem', display: 'flex', flexDirection: 'column' }} {...rest} />
 )
 
 const HorizontalStack = ({ gap, horizontalAlign, ...rest }: any) => (
-  <div style={{ display: 'flex' }} {...rest} />
+  <div style={{ padding: '1rem', display: 'flex' }} {...rest} />
 )
 
 const ContextualMenu = () => null
@@ -16,7 +17,7 @@ const buttonMenu = props => (
   <ContextualMenu {...props} items={[{ key: 'a', name: 'Item a' }, { key: 'b', name: 'Item b' }]} />
 )
 
-const wordCustomizations = {
+const word = {
   settings: {
     theme: {
       typography: {
@@ -52,119 +53,85 @@ const wordCustomizations = {
   },
 }
 
-const teamsButtonVariables = {
-  borderRadius: 2,
-  borderWidth: 2,
-  borderColor: 'rgb(189, 189, 189)',
-  iconSize: 15,
-  iconWeight: 700,
-  states: {
-    disabled: {
-      borderColor: 'inherit',
-    },
-  },
-}
-
-const teamsCustomizations = {
-  settings: {
-    theme: {
-      palette: {
-        themePrimary: '#6061aa',
-        themeLighterAlt: '#f7f7fc',
-        themeLighter: '#e1e1f2',
-        themeLight: '#c7c8e6',
-        themeTertiary: '#9797cd',
-        themeSecondary: '#6f70b5',
-        themeDarkAlt: '#56579a',
-        themeDark: '#494a82',
-        themeDarker: '#363660',
-        neutralLighterAlt: '#f8f8f8',
-        neutralLighter: '#f4f4f4',
-        neutralLight: '#eaeaea',
-        neutralQuaternaryAlt: '#dadada',
-        neutralQuaternary: '#d0d0d0',
-        neutralTertiaryAlt: '#c8c8c8',
-        neutralTertiary: '#b6b0b0',
-        neutralSecondary: '#9f9797',
-        neutralPrimaryAlt: '#877f7f',
-        neutralPrimary: '#282424',
-        neutralDark: '#585151',
-        black: '#403b3b',
-        white: '#fff',
-      },
-      semanticColors: {
-        buttonBackground: 'inherit',
-        borderWidth: 2,
-        buttonBackgroundHovered: 'rgb(189, 189, 189)',
-        buttonBackgroundPressed: 'rgb(189, 189, 189)',
-      },
-    },
-  },
-
-  scopedSettings: {
+const fabric = {
+  componentVariables: {
     Button: {
-      styleVariables: teamsButtonVariables,
+      primaryHoverBackground: '#45E',
     },
   },
-}
+
+  componentStyles: {
+    Button: {
+      root: ({ props }) => ({
+        fontWeight: 'bold',
+      }),
+
+      content: {
+        color: 'red',
+      },
+
+      icon: {
+        color: 'red',
+      },
+    },
+  },
+} as IThemeInput
 
 const buttonGap = 10
 const headingGap = 10
 
-export default class ButtonTheme extends React.Component<any> {
-  render() {
-    const { className, title, customizations } = this.props
+const ButtonSet = ({ className, title, theme = {} }: any) => {
+  return (
+    <VerticalStack className={className} gap={headingGap}>
+      <Text as="h3">{title}</Text>
 
-    return (
-      <VerticalStack className={className} gap={headingGap}>
-        <Text variant="h3">{title}</Text>
-
+      <Provider theme={theme}>
         <div>
-          {/* TODO Customizer => Provider */}
-          <div {...customizations}>
-            <VerticalStack gap={buttonGap}>
-              <HorizontalStack gap={buttonGap}>
-                <Button content="Default button" />
-                <Button disabled content="Disabled default button" />
-                <Button variant="primary" content="Primary button" />
-                <Button disabled variant="primary" content="Primary disabled button" />
-              </HorizontalStack>
+          <VerticalStack gap={buttonGap}>
+            <HorizontalStack gap={buttonGap}>
+              <Button content="Default button" />
+              <Button disabled content="Disabled default button" />
+              <Button variant="primary" content="Primary button" />
+              <Button disabled variant="primary" content="Primary disabled button" />
+            </HorizontalStack>
 
-              <HorizontalStack gap={buttonGap}>
-                <Button icon="PeopleAdd" circular />
-                <Button icon="Video" circular />
-                <Button icon="Phone" circular disabled />
-                <Button icon="FontSize" type="primary" circular />
-                <Button icon="Attach" type="primary" circular disabled />
-              </HorizontalStack>
+            <HorizontalStack gap={buttonGap}>
+              <Button icon="add user" circular />
+              <Button icon="video" circular />
+              <Button icon="phone" circular disabled />
+              <Button icon="font" type="primary" circular />
+              <Button icon="attach" type="primary" circular disabled />
+            </HorizontalStack>
 
-              <HorizontalStack gap={buttonGap}>
-                <Button icon="Upload" content="Button with string icon" />
-                <Button icon={{ iconName: 'Share' }} content="Button with iconProps" />
-                <Button icon={<Icon iconName="Download" />} content="Button with custom icon" />
-              </HorizontalStack>
+            <HorizontalStack gap={buttonGap}>
+              <Button icon="upload" content="Button with string icon" />
+              <Button
+                icon={{ name: 'share', styles: { root: { transform: 'rotate(-30deg)' } } }}
+                content="Button with iconProps"
+              />
+              <Button icon={<Icon name="download" />} content="Button with custom icon" />
+            </HorizontalStack>
 
-              <HorizontalStack gap={buttonGap}>
-                <Button>
-                  <Icon iconName="Upload" />
-                  {/*<Text variant="button">I am text.</Text>*/}
-                </Button>
+            <HorizontalStack gap={buttonGap}>
+              <Button>
+                <Icon name="upload" />
+                {/*<Text variant="button">I am text.</Text>*/}
+              </Button>
 
-                <Button variant="primary" disabled>
-                  {/*<Text variant="button">I am a variant="primary" button with text.</Text>*/}
+              <Button variant="primary" disabled>
+                {/*<Text variant="button">I am a variant="primary" button with text.</Text>*/}
+                <Icon name="upload" />
+              </Button>
+            </HorizontalStack>
 
-                  <Icon iconName="Upload" />
-                </Button>
-              </HorizontalStack>
+            <HorizontalStack gap={buttonGap}>
+              <Button content="Menu button" menu={buttonMenu} />
+              <Button disabled content="Menu disabled button" menu={buttonMenu} />
+              <Button expanded content="Menu expanded button" />
+            </HorizontalStack>
 
-              <HorizontalStack gap={buttonGap}>
-                <Button content="Menu button" menu={buttonMenu} />
-                <Button disabled content="Menu disabled button" menu={buttonMenu} />
-                <Button expanded content="Menu expanded button" />
-              </HorizontalStack>
-
-              <HorizontalStack gap={buttonGap}>
-                {/*
+            <HorizontalStack gap={buttonGap}>
+              {/*
                 <Button icon="Share" menu={buttonMenu}>
                   <VerticalStack gap={4} horizontalAlign="left">
                     <Text variant="button">I am a compound multiline button.</Text>
@@ -174,13 +141,48 @@ export default class ButtonTheme extends React.Component<any> {
                 </Button>
                 */}
 
-                <Button disabled content="Menu disabled button" menu={buttonMenu} />
-                <Button expanded content="Menu expanded button" />
-              </HorizontalStack>
-            </VerticalStack>
-          </div>
+              <Button disabled content="Menu disabled button" menu={buttonMenu} />
+              <Button expanded content="Menu expanded button" />
+            </HorizontalStack>
+          </VerticalStack>
         </div>
-      </VerticalStack>
+      </Provider>
+    </VerticalStack>
+  )
+}
+
+const allThemes = {
+  teams: themes.teams,
+  word,
+  fabric,
+}
+
+export default class ButtonTheme extends React.Component<any> {
+  state = {
+    theme: themes.teams,
+  }
+
+  switchTheme = (name: string) => () => {
+    this.setState({ theme: allThemes[name] })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Theme Switcher</h1>
+        {Object.keys(allThemes).map(themeName => (
+          <Button key={themeName} content={themeName} onClick={this.switchTheme(themeName)} />
+        ))}
+        <hr />
+        <Provider theme={this.state.theme}>
+          <ButtonSet title="Default" />
+        </Provider>
+        <hr />
+        <h1>All Themes</h1>
+        <ButtonSet title="Default" />
+        <ButtonSet title="Fabric" theme={fabric} />
+        <ButtonSet title="Word" theme={word} />
+      </div>
     )
   }
 }
