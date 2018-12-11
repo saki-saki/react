d2df2e95a35da93c56e7dfb83592f056b3375e6e
Levi Thomason <me@levithomason.com>
2018-12-11 14:36:51 -0800

    chore: replace ts-lint with eslint

--- test/jest.config.strict.js
+++ test/jest.config.strict.js
@@ -1,6 +1,6 @@
 const commonConfig = require('./jest.config.common')
 
-module.exports = Object.assign({}, 
-    commonConfig, 
-    { setupTestFrameworkScriptFile: `${__dirname}/setup.strict.ts` }
-)
+module.exports = {
+  ...commonConfig,
+  setupTestFrameworkScriptFile: `${__dirname}/setup.strict.ts`,
+}
